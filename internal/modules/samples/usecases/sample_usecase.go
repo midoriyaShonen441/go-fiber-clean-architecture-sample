3cc60de8c8fed1d9ea1edaa290696f5b341cf7c1
Midoriya-Shonen <61150735+midoriyaShonen441@users.noreply.github.com>
2024-08-04 18:49:50 +0700

    fix: recursion bug in sample_usecase.go

--- internal/modules/samples/usecases/sample_usecase.go
+++ internal/modules/samples/usecases/sample_usecase.go
@@ -1,15 +1,19 @@
 package usecases
 
-import "github.com/midoriyaShonen441/go-fiber-clean-architecture-sample/internal/entities"
+import (
+	"github.com/midoriyaShonen441/go-fiber-clean-architecture-sample/internal/entities"
+)
 
 type SampleUseCase struct {
 	Repo entities.SampleRepo
 }
 
 func NewSampleUseCase(repo entities.SampleRepo) entities.SampleUseCase {
-	return &SampleUseCase{Repo: repo}
+	return &SampleUseCase{
+		Repo: repo,
+	}
 }
 
 func (s *SampleUseCase) GetSample() (*entities.SampleRes, error) {
-	return s.GetSample()
+	return s.Repo.GetSample()
 }
